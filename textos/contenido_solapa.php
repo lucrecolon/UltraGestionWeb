d0dbec96f7062c374392e87115f15ad420ad3110
lucrecolon <lucrecolon1828@gmail.com>
2026-01-29 13:16:32 -0300

    modifico pantalla LaEmpresa

--- textos/contenido_solapa.php
+++
@@ -1,5 +0,0 @@
-<?php
-print "<br><center><img src=\"img/logos/ppypl.gif\"></center>";
-print "<br><br>";
-print "<center><img src=\"img/logos/logocus3d.jpg\"></center>";
-?>
--- textos/s_clientes_demo.php
+++
@@ -1,8 +0,0 @@
-<?php
-$_SESSION['session_descarga_rutalocal']="C:/wamp/www"; 
-$_SESSION['session_descarga_rutawww']="ultraweb/scdcus/demo";
-echo "<script>";
-echo "javascript:location.href = \"bajar.php?archivo=WinUltraSetup.exe\"";
-echo "</script>";
-?>
-
